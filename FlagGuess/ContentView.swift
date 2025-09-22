4acb528abf89e6405488a2ff6a5caba23a518275
Fahime <nikkhah.fahimeh@gmail.com>
2025-09-22 16:28:53 +0330

    Initial Commit

---
+++ FlagGuess/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlagGuess
+//
+//  Created by FaNik on 2025-09-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlagGuess/FlagGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuessApp.swift
+//  FlagGuess
+//
+//  Created by FaNik on 2025-09-22.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
